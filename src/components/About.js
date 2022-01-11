89ce547955b085e433a47a04cef1d64e2dfcd1b6
ryankissoon <ryankissoon_31@msn.com>
2022-01-11 13:01:54 -0500

    commit tree df966e7f86276030380149cb10acc16592def88a

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { username } from "../data/user";
+import {image} from "../data/user.js"
 
 function About() {
   return (
@@ -9,3 +11,5 @@ function About() {
     </div>
   );
 }
+
+export default About;
